b4b263753f1978974edfc32ae895fbecb0c34f60
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-08-27 23:14:18 +0530

    ✨ (wip): codegen `URLSession` configs

---
+++ lib/codegen/swift/urlsession.dart
@@ -0,0 +1,14 @@
+import '../../models/models.dart';
+
+class SwiftURLSessionCodeGen {
+  SwiftURLSessionCodeGen();
+
+  String? getCode(
+    HttpRequestModel requestModel,
+  ) {
+    return null;
+  }
+
+  /// Will return a String (go/http.dart & dart/dio.dart) as refs
+  void generateSwiftURLSessionCode() {}
+}
